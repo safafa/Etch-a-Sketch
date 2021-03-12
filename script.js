5ab3bd0eefea7a19d861d5f5a7cbea08b5e6c5cc
safafa <safaaballagh97@gmail.com>
2021-03-12 12:24:41 +0100

    dynamic grid feature

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+var Grid;
 function squareDiv(){
     var div = document.createElement("div");
     div.setAttribute("class","square");
-    div.appendChild(document.createTextNode('hello'));
     return div;
 }
 
-function createGrid(){
+function sizeButton(){
+    var btn = document.createElement('button');
+    btn.textContent ='Number of boxes';
+    btn.style.color = 'black';
+    btn.setAttribute('onclick','{getNumber()}');
+    return btn;  
+}
+function getNumber(){
+    number = prompt("Please enter number between 1 and 100 ", "16");
+     if(number <= 0 || number > 100){
+         getNumber();
+        }else{
+            New = createGrid(number); 
+            document.body.replaceChild(New , Grid);
+             Grid = New;
+             }
+}
+
+function createGrid(number){
     var container = document.createElement("div");
     container.setAttribute("id","container");
     title.appendChild(text);
     var Div =  document.createElement("div");
     Div.setAttribute("class","grid");
     Div.setAttribute("id","grid");
-    for(var i=0;i<256;i++){
+    Div.style.gridTemplateColumns = "repeat(" + number +", auto)";
+    Div.style.gridTemplateRows = " auto";
+    for(var i=0;i<number * number;i++){
        Div.appendChild(squareDiv());
     }
 container.appendChild(Div);
@@ -21,4 +41,6 @@ return container;
 var title = document.createElement("h4");
 var text = document.createTextNode('The ultimate grid');
 document.body.appendChild(title);
-document.body.appendChild(createGrid());
+document.body.appendChild(sizeButton());
+Grid = createGrid(16);
+document.body.appendChild(Grid);
